9d9a82f9f63d228a68d95fcca84485396f212830
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-26 20:01:55 +0300

    + функции создания UI

--- Quiz/Quiz/Helpers/MakeElement.swift
+++ Quiz/Quiz/Helpers/MakeElement.swift
@@ -5,4 +5,23 @@
 //  Created by Nikolai Maksimov on 26.12.2023.
 //
 
-import Foundation
+import UIKit
+
+class MakeElement {
+    
+    static func makeLabelView(withChar text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+//        label.font = font
+        return label
+    }
+    
+    static func makeButtonView(withTitle title: String) -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration = .filled()
+        button.configuration?.title = title
+        return button
+    }
+}
