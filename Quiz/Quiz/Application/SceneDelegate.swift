114fa1134941e28a81125bbb7e4f67b53a6afa66
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-26 03:15:11 +0300

    начальная версия проекта

---
+++ Quiz/Quiz/Application/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Quiz
+//
+//  Created by Nikolai Maksimov on 26.12.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let rootViewController = IntroductionViewController()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Quiz/Quiz/Controllers/IntroductionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IntroductionViewController.swift
+//  Quiz
+//
+//  Created by Nikolai Maksimov on 26.12.2023.
+//
+
+import UIKit
+
+final class IntroductionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
