4a22a3e5cf0c624361867341b23b7ed9fc3c6d05
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-26 19:58:30 +0300

    + файл в .gitignore

---
+++ Quiz/Quiz/Controllers/Views/IntroductionView.swift
@@ -0,0 +1,8 @@
+//
+//  IntroductionView.swift
+//  Quiz
+//
+//  Created by Nikolai Maksimov on 26.12.2023.
+//
+
+import Foundation
---
+++ Quiz/Quiz/Helpers/MakeElement.swift
@@ -0,0 +1,8 @@
+//
+//  MakeElement.swift
+//  Quiz
+//
+//  Created by Nikolai Maksimov on 26.12.2023.
+//
+
+import Foundation
