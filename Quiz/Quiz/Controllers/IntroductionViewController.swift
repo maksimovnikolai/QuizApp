817d150895c606fcd9f1bf0d37cca715c6a351ec
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-29 15:51:01 +0300

    Отображение UI

--- Quiz/Quiz/Controllers/IntroductionViewController.swift
+++ Quiz/Quiz/Controllers/IntroductionViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 final class IntroductionViewController: UIViewController {
     
+    private let elementView = IntroductionView()
+    
+    override func loadView() {
+        view = elementView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        commonInit()
+    }
+}
+
+extension IntroductionViewController {
+    
+    private func commonInit() {
+        elementView.startButton.addTarget(self, action: #selector(presentQuestionView), for: .touchUpInside)
+    }
+    
+    @objc
+    private func presentQuestionView() {
+        let questionVC = QuestionsViewController()
+        let destinationVC = UINavigationController(rootViewController: questionVC)
+        destinationVC.modalPresentationStyle = .fullScreen
+        present(destinationVC, animated: true)
     }
 }
