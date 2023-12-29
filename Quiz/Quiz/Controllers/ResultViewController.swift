daad9ac5c8e1cb6b168024dad60513051de88ca7
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-29 20:32:29 +0300

    + resultViewController. отображение выбора пользователя

---
+++ Quiz/Quiz/Controllers/ResultViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ResultViewController.swift
+//  Quiz
+//
+//  Created by Nikolai Maksimov on 28.12.2023.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+    
+    var answers: [Answer]!
+    
+    private let resultView = ResultView()
+    
+    override func loadView() {
+        view = resultView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavBar()
+        updateResult()
+    }
+    
+    func configureNavBar() {
+        title = "Результат"
+        navigationItem.hidesBackButton = true
+    }
+    
+    private func updateResult() {
+        var frequencyAnimal: [Animal: Int] = [:]
+        var dogsCount = 0
+        var catsCount = 0
+        var rabbitCount = 0
+        var turtleCount = 0
+        for answer in answers {
+            switch answer.animal {
+            case .dog:
+                dogsCount += 1
+                frequencyAnimal.updateValue(dogsCount, forKey: .dog)
+            case .cat:
+                catsCount += 1
+                frequencyAnimal.updateValue(catsCount, forKey: .cat)
+            case .rabbit:
+                rabbitCount += 1
+                frequencyAnimal.updateValue(rabbitCount, forKey: .rabbit)
+            default:
+                turtleCount += 1
+                frequencyAnimal.updateValue(turtleCount, forKey: .turtle)
+            }
+        }
+        updateUI(from: frequencyAnimal)
+    }
+    
+    private func updateUI(from dict: [Animal: Int]) {
+        if let mostFrequencyAnimal = dict.max(by: { $0.value < $1.value }) {
+            resultView.animalTypeLabel.text = "Вы - \(mostFrequencyAnimal.key.rawValue)"
+            resultView.descriptionLabel.text = "\(mostFrequencyAnimal.key.definition)"
+        }
+    }
+}
